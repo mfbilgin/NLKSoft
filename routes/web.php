793322845044f71ecaeaef46f42c2ef6791859fe
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-07-02 11:32:19 +0300

    Add admin control

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,19 @@
 <?php
 
+use App\Http\Controllers\Auth\AdminController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-})->name('home');
+})->name('home')->middleware('auth');
 
-Route::get('register', [RegisterController::class, 'showRegisterPage'])->name('register');
+Route::get('register', [RegisterController::class, 'showRegisterPage'])->name('register')->middleware('guest');
 Route::post('register', [RegisterController::class, 'register']);
 
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class, 'login']);
 Route::get('logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::get('admin/dashboard', [AdminController::class, 'index'])->name('admin')->middleware('admin');
