be21d1e0812099a8dc7b84c015a4a742a8c3f196
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-07-03 19:52:34 +0300

    Add product and category crud routes

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-})->name('home')->middleware('auth');
+})->name('home');
 
 Route::get('register', [RegisterController::class, 'showRegisterPage'])->name('register')->middleware('guest');
 Route::post('register', [RegisterController::class, 'register']);
@@ -19,14 +19,23 @@ Route::get('login', [LoginController::class, 'showLoginForm'])->name('login')->m
 Route::post('login', [LoginController::class, 'login']);
 Route::get('logout', [LoginController::class, 'logout'])->name('logout')->middleware('auth');
 
-Route::get('admin/dashboard', [ProductController::class, 'showProductListPage'])->name('admin.dashboard')->middleware('admin');
+Route::get('admin/dashboard', [ProductController::class, 'showProductListPageForAdmin'])->name('admin.dashboard')->middleware('admin');
 
-Route::get('admin/product/list', [ProductController::class, 'showProductListPage'])->name('admin.product.list')->middleware('admin');
+Route::get('admin/product/list', [ProductController::class, 'showProductListPageForAdmin'])->name('admin.product.list')->middleware('admin');
 Route::get('admin/product/add', [ProductController::class, 'showProductAddPage'])->name('admin.product.add')->middleware('admin');
+Route::get('admin/product/update/{id}',[ProductController::class,'showEditProductPage'])->name('product.edit')->middleware('admin');
+Route::post('product/create',[ProductController::class,'addProduct'])->name('product.store')->middleware('admin');
+Route::delete('product/delete/{id}',[ProductController::class,'deleteProduct'])->name('product.delete')->middleware('admin');
+Route::put('product/update/{id}',[ProductController::class,'updateProduct'])->name('product.update')->middleware('admin');
 
 Route::get('admin/category/list', [CategoryController::class, 'showCategoryListPage'])->name('admin.category.list')->middleware('admin');
 Route::get('admin/category/add', [CategoryController::class, 'showCategoryAddPage'])->name('admin.category.add')->middleware('admin');
+Route::post('category/create',[CategoryController::class,'addCategory'])->name('category.store')->middleware('admin');
+Route::delete('category/delete/{id}',[CategoryController::class,'deleteCategory'])->name('category.delete')->middleware('admin');
+Route::get('admin/category/update/{id}',[CategoryController::class,'showEditCategoryPage'])->name('category.edit')->middleware('admin');
+Route::put('category/update/{id}',[CategoryController::class,'updateCategory'])->name('category.update')->middleware('admin');
 
 Route::get('admin/user/list', [UserController::class, 'showUserListPage'])->name('admin.user.list')->middleware('admin');
 Route::delete('admin/user/delete/{id}', [UserController::class, 'deleteUser'])->name('admin.user.delete')->middleware('admin');
-Route::put('admin/user/change-role/{id}/{newRole}', [UserController::class, 'changeRole'])->name('admin.user.change-role')->middleware('admin');
+Route::put('admin/user/change-role/{id}/{newRole}', [UserController::class, 'changeRole'])->name('user.change-role')->middleware('admin');
+
