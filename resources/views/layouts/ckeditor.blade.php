5e1fc36cf1861b6b6fd9eef8937412bf25bf5a8d
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-07-05 11:02:07 +0300

    Move replace to product add page

--- resources/views/layouts/ckeditor.blade.php
+++ resources/views/layouts/ckeditor.blade.php
@@ -1,3 +1,1 @@
-<script src="https://cdn.ckeditor.com/4.22.1/standard/ckeditor.js"></script><script>
-    CKEDITOR.replace("description");
-</script>
+<script src="https://cdn.ckeditor.com/4.22.1/standard/ckeditor.js"></script>
